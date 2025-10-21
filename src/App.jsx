a4969525eb580fb11718860f3909553967995953
AbdifatahDeeq <deeq8763@gmail.com>
2025-10-21 22:46:02 +0200

    exercise7

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-
-import UseEffect from "./UseEffect";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return <UseEffect />;
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  
+  useEffect(() => {
+    const MouseMove = (event) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+      
+    }
+    window.addEventListener("mousemove", MouseMove);
+    return () => {
+      window.removeEventListener("mousemove", MouseMove)
+    }
+  })
+
+
+
+
+  return (
+    <>
+      <p>Mouse X position :{position.x}</p>
+      <p>Mouse Y position :{position.y}</p>
+    </>
+  );
 }
-export default App
+export default App;
